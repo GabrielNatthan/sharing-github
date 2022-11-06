ed4afd52b6c4d639a8ada923f9c412f63ede357c
GabrielNatthan <93145914+GabrielNatthan@users.noreply.github.com>
2022-11-05 23:59:48 -0300

    Update Lista1.py

--- Lista1.py
+++ Lista1.py
@@ -1,9 +1,1 @@
-# Lista 1
 
-# Questão 1
-    # O preço de fechamento de uma ação é o último preço em que ela foi negociada numa sessão. O de fechamento
-    # ajustado levará em conta também quaisquer distribuições de proventos e ações corporativas que ocorreram
-    # em qualquer momento antes da abertura do dia seguinte. Ele possui vantagam em relação ao fechamento normal
-    # pois o preço no ajustado oferece informações sobre o valor patrimonial da empresa, ou seja, vão além do
-    # preço de mercado dela. São levados em conta no preço ajustado os eventos de desdobramento e grupamento
-    # de ações, como também levam em conta os dividendos distribuidos e a oferta de direitos
